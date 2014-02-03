8e238d14a15d9f7ad52f32a84909d8a6e8ce69cc
Love Hansson <love.hansson@pensionsmyndigheten.se>
2014-02-03 18:19:43 +0100

    Fiddle around with charts, reactivity and execution scheduling. No success so far.

--- hej2/server.R
+++ hej2/server.R
@@ -1,6 +1,10 @@
 
 shinyServer(function(input, output) {
 	
+	
+	
+	## Variable selectors (top) ----
+	
 	output$UI <- renderUI({
 		menudata <- unique(hierarchy[,list(id_lv1, desc_lv1)])
 		list(
@@ -17,6 +21,7 @@ shinyServer(function(input, output) {
 		)
 	})
 	output$UI2 <- renderUI({
+		if (is.null(input$myInput)) return()
 		menudata <- unique(hierarchy[id_lv1 %in% input$myInput, list(id_lv2, desc_lv2)])
 		list(
 			bootstrapSelectInput(
@@ -32,6 +37,7 @@ shinyServer(function(input, output) {
 		)
 	})
 	output$UI3 <- renderUI({
+		if (is.null(input$myInput2)) return()
 		menudata <- unique(hierarchy[id_lv2 %in% input$myInput2, list(id_lv3, desc_lv3)])
 		list(
 			bootstrapSelectInput(
@@ -47,6 +53,7 @@ shinyServer(function(input, output) {
 		)
 	})
 	output$UI4 <- renderUI({
+		if (is.null(input$myInput3)) return()
 		menudata <- unique(hierarchy[id_lv3 %in% input$myInput3, list(id_lv4, desc_lv4)])
 		list(
 			bootstrapSelectInput(
@@ -63,74 +70,151 @@ shinyServer(function(input, output) {
 	})
 	
 	output$goButton <- renderUI({
-		actionButton("fetchScbVar", "Hämta metadata")
+		actionButton("fetchScbVar", "Get metadata")
 	})
 	
 	
-	## Metadata ----
+	
+	## Metadata selectors (left) ----
+	
+	output$leftBar <- renderUI({
+
+		if (!is.null(input$fetchScbVar)) { if (input$fetchScbVar > 0) {
+# 			print(var_data())
+			uiOutput("metaDataSelectors")
+			uiOutput("dataButton")
+		} else {
+			return(NULL)
+		}}
+		
+	})
 	
 	output$metaDataSelectors <- renderUI({
-		getMetaData()
+		# 		print(dims)
+		if (is.null(input$fetchScbVar)) return()
+		if (input$fetchScbVar == 0) return()
+		isolate({ dims <- var_data()$dims })
+		if (is.null(dims)) return()
+
 		
 		metadataSelectorList <- lapply(dims, function(i) {
-			bootstrapSelectInput(
-				inputId = i$code,
-				label = i$code,
-				choices = c("*", i$values),
-				subtext = c("", i$valueTexts),
-				selected = "*",
-				multiple = TRUE,
-				options = list(
-					width = "120px"
+			if (i$code != "Tid") {	return(
+				bootstrapSelectInput(
+					inputId = i$code,
+					label = i$code,
+					choices = c("*", i$values),
+					subtext = c("", i$valueTexts),
+					selected = "*",
+					multiple = FALSE,
+					options = list(
+						width = "120px"
+					)
 				)
-			)
+			)} else { return(
+				sliderInput(
+					inputId = i$code,
+					label = i$code,
+					value = c(as.numeric(min(i$values)), as.numeric(max(i$values))),
+					min = as.numeric(min(i$values)),
+					max = as.numeric(max(i$values)),
+					format = "####",
+					step = 1
+				)
+			)}
 		})
 		
-		do.call(tagList, metadataSelectorList)
+		return(do.call(tagList, metadataSelectorList))
 	})
 	
 	output$dataButton <- renderUI({
-		actionButton("fetchScbData", "Hämta data")
+		actionButton("fetchScbData", "Get data")
 	})
 	
 	
-	## Data ----
-	getMetaData <- reactive({
-		datavar <<- ifelse(input$myInput4 == "", input$myInput3, input$myInput4)
-		metadata <<- scbGetMetadata(datavar)
-		dims <<- scbGetDims(metadata)
-	})
 	
-	getData <- reactive({
-		observe({
-			input$fetchScbData
-			
-			isolate({
-				datadims <- lapply(dims, function(i) {
-					input[[i$code]]
-				})
-				names(datadims) <- unlist(lapply(dims, function(i) i$code))
-				# 		print(datadims)
-				print(paste0("URL: ", metadata$URL))
-				print(datadims)
-				s_data <<- scbGetData(metadata$URL, dims = datadims, clean = TRUE)
-				# Correct for "åäö" in data header
+	## Data reactives ----
+	
+	var_data <- reactive({
+		datavar <- ifelse(input$myInput4 == "", input$myInput3, input$myInput4)
+		metadata <- scbGetMetadata(datavar)
+		dims <- scbGetDims(metadata, verbose = FALSE)
 				
+		return(list(datavar=datavar, metadata=metadata, dims=dims))
+	})
+	
+	main_data <- reactive({
+			datadims <- lapply(var_data()$dims, function(i) {
+				if (i$code != "Tid") {
+					return(input[[i$code]])
+				} else {
+					return(as.character(input[[i$code]][1]:input[[i$code]][2]))
+				}
 			})
+			names(datadims) <- unlist(lapply(var_data()$dims, function(i) i$code))
+			# 		print(datadims)
+# 			print(paste0("URL: ", var_data()$metadata$URL))
+# 			print(datadims)
+			
+			scbData <- scbGetData(var_data()$metadata$URL, dims = var_data()$datadims, clean = TRUE)
+			setnames(scbData, names(scbData)[ncol(scbData)], "varde")
+			s_data <- copy(scbData)
+			# Correct for "åäö" in data header
+		
+		return(s_data)
+		
+	})
+	
+	observe({
+		if (is.null(input$fetchScbData)) return()
+		if (input$fetchScbData == 0) return()
+		
+		input$fetchScbData
+# 		print(input$fetchScbData)
+		main_data()
+	})
+	
+	observe({
+		if (is.null(input$fetchScbVar)) return()
+		if (input$fetchScbVar == 0) return()
+		
+		input$fetchScbVar
+# 		print(input$fetchScbVar)
+		var_data()
+	})
+	
+	
+	## Graph (center) ----
+	output$myChart <- renderUI({
+		if (!is.null(input$fetchScbData)) { if (input$fetchScbData > 0) {
+			return(showOutput("rChart", "polycharts"))
+		}} else {
+			return(textOutput("chartText"))
+		}
+	})
+	
+	output$rChart <- renderChart({
+		isolate({
+			p1 <- rPlot("tid", "varde", data = main_data(), color = "variabel", 
+						facets = "region", type = 'line')
+			p1$addParams(dom = 'myChart')
+			p1
 		})
 	})
 	
-	
-	## Graph ----
-	output$myChart <- renderChart({
-		if (!is.null(input$fetchScbData)) { if (input$fetchScbData > 0) {
-			getData()
-			print(s_data)
-			p1 <- rPlot("värde", "tid", data = s_data, color = "variabel", 
-						facet = "region", type = 'point')
-			p1$addParams(dom = 'myChart')
-			return(p1)
-		}}
-		NULL
+	output$chartText <- renderPrint({
+		'3. Click the "Get Data" button to display a graph.'
 	})
+		
+	
+# 	output$myChart <- renderPlot({
+# 		if (!is.null(input$fetchScbData)) { if (input$fetchScbData > 0) {
+# 			getData()
+# 			print(s_data)
+# 			
+# 			p <- ggplot(s_data, aes_string(x="tid", y="varde", fill="variabel"
+# 			)) + geom_point()
+# 			print(p)
+# 			
+# 		}}
+# 	})
 })
